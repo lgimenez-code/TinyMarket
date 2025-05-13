b3569e2fa69798146e6ebbfc5787151b079a2030
lgimenez-code <lucasgimenez2007@gmail.com>
2025-05-13 04:17:36 -0300

    maquetado

---
+++ TinyMarket/wwwroot/app.js
@@ -0,0 +1,17 @@
+﻿
+
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const addProductBtn = document.getElementById('add-product');
+const productModal = document.getElementById('product-modal');
+
+
+
+hamburgerMenu.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
+
+addProductBtn.addEventListener('click', () => {
+    document.getElementById('modal-title').textContent = 'Añadir Producto';
+    document.getElementById('product-id').value = '';
+    productModal.classList.add('active');
+});
